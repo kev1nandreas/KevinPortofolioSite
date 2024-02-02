1b47b7ffaf25251cba9e70a85b92512c7cc1f935
kev1nandreas <kevin.andreascn@gmail.com>
2024-02-02 16:48:52 +0700

    Concept of Experience and Touch Up

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            isSmall: true,
+            navBar: false,
         }
     },
     computed: {
-        isSmall() {
-            return this.windowWidth <= 1024;
-            console.log(this.isSmall);
-        },
+        
     },
 }).mount('#app');
