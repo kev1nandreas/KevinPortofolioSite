1a8c4c5621eeeb5517d781a66f245f8c9c988933
kev1nandreas <kevin.andreascn@gmail.com>
2024-02-03 00:29:49 +0700

    Finishing experience section

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ createApp({
     data() {
         return {
             navBar: false,
+            maxHeight1: true,
+            maxHeight2: true,
+            maxHeight3: true,
+            maxHeight4: true,
+            maxHeight5: true,
+            maxHeight6: true,
+            maxHeight7: true,
+
         }
     },
     computed: {
